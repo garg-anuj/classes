f70eec656d27a1bffad2c900633e0da584019fba
adsRole-garg-anuj <159473985+adsRole-garg-anuj@users.noreply.github.com>
2024-10-25 11:07:04 +0530

    dom starting

---
+++ javaScript/script.js
@@ -0,0 +1,18 @@
+let value = {
+  myObj: [1, 2, 3, 4, 4],
+};
+
+let targetFirstValueInputBox = document.getElementById("firstValue");
+console.log(targetFirstValueInputBox.value);
+let targetSubmitBtn = document.getElementById("submitBtn");
+// ev
+
+console.log("Hello Java Script", 2 * 5, value);
+
+// 1st inputs ko target karo  2 dono input target hone ke baad
+//  button ko target kro
+// btn trget krne ke baad isPr functionaality bnao ki yeah value kaa
+//  sum krlee
+
+// or fir  Result show kr de result me outPut show krne ke liye hme Resule
+// wali place ko bhi target krna pdega
